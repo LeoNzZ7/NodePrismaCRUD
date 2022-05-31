73d1bc343e0d4248ef2daf82f77b56fcf89809e7
Leonardo Nunes Martinha <Leonardomartinha.dev@gmail.com>
2022-05-31 15:49:12 -0300

    Início

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { routes } from './routes/routes';
+
+const server = express();
+
+server.use(express.json());
+server.use(routes);
+
+server.listen('3000', () => {
+   console.log('HTTP server runing!');
+});
